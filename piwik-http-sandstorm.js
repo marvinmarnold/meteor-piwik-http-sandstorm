b32b544751d35b6987e789a89fa42496de3cd85f
Marvin Arnold <marvinmarnold@gmail.com>
2016-08-22 21:06:28 -0500

    Move logic to server

--- piwik-http-sandstorm.js
+++ piwik-http-sandstorm.js
@@ -2,24 +2,43 @@ import { Meteor } from 'meteor/meteor';
 import { HTTP } from 'meteor/http';
 import { Random } from 'meteor/random';
 
-// Notify Piwik that a specific URL has been visited
-const track = (params, callback) => {
-  const endpoint = Meteor.settings.public.PIWIK.url
+if(Meteor.isServer) {
+  Meteor.methods({
+    'piwik.track': function(params) {
+      const endpoint = Meteor.settings.PIWIK.url
 
-  _.defaults(params, {
-    idsite: 1,
-    rec: 1,
-    apiv: 1
-  })
+      if(endpoint) {
+        _.defaults(params, {
+          idsite: 1,
+          rec: 1,
+          apiv: 1,
+          cip: this.connection.clientAddress,
+          ua: this.connection.httpHeaders["user-agent"],
+          urlref: this.connection.httpHeaders["referer"],
+          lang: this.connection.httpHeaders["accept-language"],
+          rand: Random.id(),
+          _id: Meteor.userId()
+        })
+        console.log(params);
+        console.log(endpoint);
 
-  if(endpoint) {
-    return HTTP.get(endpoint, {
-      headers: {"Authorization": "Bearer " + Meteor.settings.public.PIWIK.sandstormAuth},
-      params
-    }, callback)
-  }
+        HTTP.get(endpoint, {
+          headers: {"Authorization": "Bearer " + Meteor.settings.PIWIK.sandstormAuth},
+          params,
+        }, (err, resp) => {
+          if(!err) {
+            console.log("Visit tracked on " + endpoint + " to " + params.url);
+          } else {
+            console.log("Error tracking");
+            console.log(err);
+            console.log(err.stack);
+          }
+        })
+      }
 
-  return callback(undefined, {})
+      return {}
+    }
+  });
 }
 
 // FlowRouter triggersEnter hook to track
@@ -27,22 +46,16 @@ const flowTrackEnter = (context) => {
   // Don't track if page has not changed
   if(context.oldRoute &&
     (context.oldRoute.name === context.route.name))
-    return
+    return null
 
   const params = {
     url: Meteor.absoluteUrl(context.path.replace(/^\//,"")),
     action_name: context.route.name,
-    _id: Meteor.userId(),
-    rand: Random.id()
   }
 
-  return track(params, (err, resp) => {
-    if(!err)
-      console.log("Visit tracked on " + Meteor.settings.public.PIWIK.url + " to " + params.url);
-  })
+  Meteor.call("piwik.track", params)
 }
 
 export const piwik = {
-  track,
   flowTrackEnter
 }
