e0366d7a26976fc995b7c7862cefdffee7f02291
Marvin Arnold <marvinmarnold@gmail.com>
2016-09-07 16:07:14 -0500

    allow custom actions to be tracked

--- piwik-http-sandstorm.js
+++ piwik-http-sandstorm.js
@@ -17,15 +17,17 @@ if(Meteor.isServer) {
         urlref: thiz.connection.httpHeaders["referer"],
         lang: thiz.connection.httpHeaders["accept-language"],
         rand: Random.id(),
-        _id: Meteor.userId()
+        _id: Meteor.userId(),
+        token_auth: Meteor.settings.PIWIK.tokenAuth
       }
-      console.log(defaultParams);
+      // console.log(defaultParams);
 
       if(endpoint) {
         _.defaults(params, defaultParams)
+        console.log(params);
 
         HTTP.get(endpoint, {
-          headers: {"Authorization": "Bearer " + Meteor.settings.PIWIK.sandstormAuth},
+          // headers: {"Authorization": "Bearer " + Meteor.settings.PIWIK.sandstormAuth},
           params,
         }, (err, resp) => {
           if(!err) {
@@ -37,7 +39,6 @@ if(Meteor.isServer) {
           }
         })
       }
-
       // console.log("Visit not tracked because PIWIKI.url is empty.");
       return {}
     }
@@ -46,10 +47,14 @@ if(Meteor.isServer) {
 
 // FlowRouter triggersEnter hook to track
 const flowTrackEnter = (context) => {
+  // console.log('flowTrackEnter');
+  // console.log(context.route.name);
+  // console.log(context.oldRoute);
+
   // Don't track if page has not changed
-  if(context.oldRoute &&
-    (context.oldRoute.name === context.route.name))
-    return null
+  // if(context.oldRoute &&
+  //   (context.oldRoute.name === context.route.name))
+  //   return null
 
   const params = {
     url: Meteor.absoluteUrl(context.path.replace(/^\//,"")),
@@ -59,6 +64,16 @@ const flowTrackEnter = (context) => {
   Meteor.call("piwik.track", params)
 }
 
+const trackAction = (path, action) => {
+  const params = {
+    url: Meteor.absoluteUrl(path),
+    action_name: action,
+  }
+
+  Meteor.call("piwik.track", params)
+}
+
 export const piwik = {
-  flowTrackEnter
+  flowTrackEnter,
+  trackAction
 }
